4dfb876ab1859d150d6c47a25c62f9ae07c03c23
Anas Elkadiri <aelkadir@e3r7p9.1337.ma>
2025-07-28 20:10:36 +0100

    end-to-end working

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
+docker-compose build 
+docker-compose ps 
+docker-compose up -d 
+
 docker exec hadoop hdfs dfs -rm -r -f /input
 docker exec hadoop hdfs dfs -rm -r -f /output
 docker exec hadoop hdfs dfs -mkdir /input 
 docker exec hadoop hdfs dfs -put /data/movies.csv /input 
 docker exec hadoop ./data/namenode_entrypoint.sh
 ./visualise/extract.c
+
+docker-compose down 
